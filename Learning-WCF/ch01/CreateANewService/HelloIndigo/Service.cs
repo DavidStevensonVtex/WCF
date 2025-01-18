7dd9142b65e904814c1792bee38c994911809c95
David Stevenson <drstevenson1958@yahoo.com>
2025-01-18 18:50:06 -0500

    Add ServiceContract and OperationContract attributes to the IHelloIndigoService interface and HelloIndigo method.

--- Learning-WCF/ch01/CreateANewService/HelloIndigo/Service.cs
+++ Learning-WCF/ch01/CreateANewService/HelloIndigo/Service.cs
@@ -1,7 +1,11 @@
-﻿namespace HelloIndigo
+﻿using System.ServiceModel;
+
+namespace HelloIndigo
 {
+    [ServiceContract(Namespace="http://www.thatindigogirl.com/samples/2006/06")]
     internal interface IHelloIndigoService
     {
+        [OperationContract]
         string HelloIndigo();
     }
 }
