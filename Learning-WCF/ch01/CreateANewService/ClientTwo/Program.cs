0d74d86b671e7b563d5b89aef01fcf969f55e973
David Stevenson <drstevenson1958@yahoo.com>
2025-01-19 13:56:24 -0500

    Learning WCF: Chapter 1: Generating a proxy using the Service Model Metadata Utility: Implement Program.cs using the newly generated proxy

--- Learning-WCF/ch01/CreateANewService/ClientTwo/Program.cs
+++ Learning-WCF/ch01/CreateANewService/ClientTwo/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace ClientTwo
+﻿using System;
+
+namespace ClientTwo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            using (HelloIndigoServiceClient proxy = new HelloIndigoServiceClient())
+            {
+                string s = proxy.HelloIndigo();
+                Console.WriteLine(s);
+                Console.WriteLine("Press <Enter> to exit program.");
+                Console.ReadLine();
+            }
         }
     }
 }
